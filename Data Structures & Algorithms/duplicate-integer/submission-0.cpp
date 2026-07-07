bac6bd4241287565d9be48586451ee348449aae8
VSladojevic <sladojevicv@gmail.com>
2026-07-07 14:22:56 +0200

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> distinct;
+        for( int x: nums)
+        {
+            if(distinct.find(x)!=distinct.end())
+            {
+                // Found
+                return true;
+            }
+            else
+            {
+                distinct.insert(x);
+            }
+        }
+
+        return false;
+    }
+};
